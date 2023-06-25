43f2b241da0085c59f045f68f04e024f4f039e58
jp <jp@jps-MacBook-Pro.local>
2023-06-25 20:42:35 -0300

    Ajustes

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -1,13 +1,23 @@
-import { log } from './log.mjs'
+import { log } from './log.mjs';
+import { S3 } from '@aws-sdk/client-s3';
 
+const s3Client = new S3({ region: 'us-east-2' });
 export const handler = async(event) => {
-    log('Event :' + JSON.stringify(event));
-    
-    return {
-        statusCode: 200,
-        body: `<html><body>Dados da Requisicao Atual: ${JSON.stringify(event)}</body></html>`,
-        headers:  {
-            "content-type": "text/html"
-        }
-    };
+    const record = event.Records[0];
+    const Bucket = record.s3.bucket.name;
+    const Key = record.s3.object.key;
+    const getObjectResult = await s3Client.getObject({
+        Bucket,
+        Key,
+    });
+    const mega_byte = 1024 * 1024;
+
+    if (getObjectResult.ContentLength > 1 * mega_byte) {
+        log('Objeto muito grande');
+
+        return 'Objeto muito grande';
+    }
+
+    log('Objeto de tamanho OK');
+    return 'Objeto de tamanho OK';
 };
