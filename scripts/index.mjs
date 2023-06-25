bf37bb3827e7e369e8f822d9cc4cc7750b5e5565
jp <jp@jps-MacBook-Pro.local>
2023-06-25 17:49:56 -0300

    Ajustes para testes

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -1,11 +1,11 @@
-import { log } from '../log.mjs'
+import { log } from './log.mjs'
 
 export const handler = async(event) => {
     log('Event :' + JSON.stringify(event));
     
     return {
         statusCode: 200,
-        body: `<html><body>Dados da Requisicao: ${JSON.stringify(event)}</body></html>`,
+        body: `<html><body>Dados da Requisicao Atual: ${JSON.stringify(event)}</body></html>`,
         headers:  {
             "content-type": "text/html"
         }
